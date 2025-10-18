1870fa93605968fb8ace5bb664687d7565bab9e1
Mikhail Poliakov <mikhail.polyakov.2006@bk.ru>
2025-10-18 18:22:32 +0300

    Added buttons on Admin form

--- PR08_GIT_Polaikov_Karaseva/AdminForm.Designer.cs
+++ PR08_GIT_Polaikov_Karaseva/AdminForm.Designer.cs
@@ -41,6 +41,10 @@ namespace PR08_GIT_Polaikov_Karaseva
             this.textBox4 = new System.Windows.Forms.TextBox();
             this.label5 = new System.Windows.Forms.Label();
             this.textBox5 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -145,12 +149,56 @@ namespace PR08_GIT_Polaikov_Karaseva
             this.textBox5.Size = new System.Drawing.Size(417, 44);
             this.textBox5.TabIndex = 9;
             // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Chartreuse;
+            this.button1.Location = new System.Drawing.Point(644, 328);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(265, 33);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Добавить";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Chartreuse;
+            this.button2.Location = new System.Drawing.Point(644, 382);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(265, 33);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Редактировать";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Chartreuse;
+            this.button3.Location = new System.Drawing.Point(644, 431);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(265, 33);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "Удалить";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Chartreuse;
+            this.button4.Location = new System.Drawing.Point(644, 487);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(265, 33);
+            this.button4.TabIndex = 14;
+            this.button4.Text = "На главную";
+            this.button4.UseVisualStyleBackColor = false;
+            // 
             // AdminForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(974, 571);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.textBox5);
             this.Controls.Add(this.label3);
@@ -185,5 +233,9 @@ namespace PR08_GIT_Polaikov_Karaseva
         private System.Windows.Forms.TextBox textBox4;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
